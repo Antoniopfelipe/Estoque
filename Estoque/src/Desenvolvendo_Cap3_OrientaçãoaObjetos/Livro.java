de69f990b0cb3f95a0ce1f55add097480fed45bd
Antoniopfelipe <antonio.nba10@gmail.com>
2019-03-31 16:47:51 -0300

    Add new book and agradecimentos

---
+++ Estoque/src/Desenvolvendo_Cap3_OrientaçãoaObjetos/Livro.java
@@ -0,0 +1,10 @@
+package Desenvolvendo_Cap3_OrientaçãoaObjetos;
+
+public class Livro {
+   String nome;
+   String descricao;
+   double valor;
+   String isbn;
+   Livro livro = new Livro();
+		   }
+   
